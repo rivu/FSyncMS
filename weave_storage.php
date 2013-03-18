ae89b5ac1a7d9b7d9868e994a1e06e4cfe56b72c
tobiashollerung <tobias@hollerung.eu>
2013-03-19 00:11:16 +0100

    Update weave_storage.php
    added Contribution notice

--- weave_storage.php
+++ weave_storage.php
@@ -22,6 +22,7 @@
 #
 # Contributor(s):
 #	Toby Elliott (telliott@mozilla.com)
+#	Tobias Hollerung (tobias@hollerung.eu)
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the 'GPL'), or
